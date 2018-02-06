2f23b3479d066a37500d67c8d899d68569271ecc
sergiohere1 <sergio11garcia@hotmail.com>
2018-02-06 10:01:34 +0100

    Terminado, pulsar botones interactivos, coger objetos, detectar distancias...

---
+++ LeapMotion/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+	//Referenci al componente AudioSource
+	private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		//Recuperamos la referencia al componente audioSource
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Click(){
+		audioSource.Play ();
+	}
+}
